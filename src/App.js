6c2c62e370350b830eef3334b3c8b46814826611
Bushiran Ibrahim Witol <80077495+Bushiran@users.noreply.github.com>
2022-03-24 15:06:40 +0000

    profile-maker

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import ProfileForm from './components/ProfileForm'
+import ProfileCard from './components/ProfileCard'
+
+function App() {
+  const[allProfiles, setAllProfiles]=useState([
+    {
+      firstname: "Ama",
+      lastname: 'Mensah',
+      email: "mensah@gmail.com",
+      phone: "0201847659"
+    }
+  ])
+
+  const updateProfiles = (profile)=>{
+    let arr =allProfiles
+    arr.push(profile)
+    setAllProfiles([...arr]);
+  };
+
+  return (
+    <div className='app'>
+    <h1>Profile Maker</h1>
+    <div>
+    <ProfileForm submit ={updateProfiles}/>
+    <hr/>
+    <div className='list'>
+    {allProfiles.map((person, index)=>(
+      <ProfileCard key={index} card={person}/>
+    ))}
+    </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default App
